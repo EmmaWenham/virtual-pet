f48ae6c617859f63a3ac965da9f6aecd97235ddc
Emma Wenham <ewenham3@gmail.com>
2022-04-13 19:10:46 +0100

    added hunger and fitness tests

--- src/pet.js
+++ src/pet.js
@@ -1,9 +1,19 @@
 function Pet(name) {
+    
+    this.hunger = 0;
+    this.fitness = 10;
     this.name = name;
     this.age = 0;
+
     this.growUp = function() {
         this.age += 1;
+        this.hunger += 5;
+        this.fitness -= 3;
     };
+
 }
+
+const fido = new Pet('Fido');
+const rex = new Pet('Rex');
 
 module.exports = Pet;
